b8c22099b27fe8eb8f2550d831dbb12c466b5644
Nguyen Ngoc Manh <57675858+ngmanh47@users.noreply.github.com>
2021-06-04 20:47:53 +0700

    update database

--- academy.sql
+++ academy.sql
@@ -1,199 +1,121 @@
--- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
---
--- Host: localhost    Database: academy
--- ------------------------------------------------------
--- Server version	8.0.25
+/*
+ Navicat Premium Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 100410
+ Source Host           : localhost:3306
+ Source Schema         : academy
 
---
--- Table structure for table `academy`
---
+ Target Server Type    : MySQL
+ Target Server Version : 100410
+ File Encoding         : 65001
 
+ Date: 04/06/2021 20:45:44
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for academy
+-- ----------------------------
 DROP TABLE IF EXISTS `academy`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `academy` (
-  `academy_id` int NOT NULL AUTO_INCREMENT,
-  `academy_category_id` int DEFAULT NULL,
-  `academy_name` varchar(100) NOT NULL,
-  `avatar` varchar(100) DEFAULT NULL,
-  `description_short` varchar(100) DEFAULT NULL,
-  `description_detail` text,
-  `price` bigint NOT NULL,
-  `status` varchar(10) DEFAULT NULL,
-  `teacher_id` int NOT NULL,
-  `promotion` varchar(100) DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`academy_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `academy`  (
+  `academy_id` int(11) NOT NULL AUTO_INCREMENT,
+  `academy_category_id` int(11) NOT NULL,
+  `academy_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatar` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `description_short` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `description_detail` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
+  `price` bigint(20) NOT NULL COMMENT 'Giá khoá học',
+  `status` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `teacher_id` int(11) NOT NULL COMMENT 'Mã giáo viên',
+  `promotion` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT 'Khuyến mãi',
+  `is_delete` tinyint(1) NULL DEFAULT 0 COMMENT '0: Chưa xoá, 1: Đã xoá',
+  `created_at` timestamp(0) NULL DEFAULT NULL,
+  `updated_at` timestamp(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`academy_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `academy`
---
-
-LOCK TABLES `academy` WRITE;
-/*!40000 ALTER TABLE `academy` DISABLE KEYS */;
-/*!40000 ALTER TABLE `academy` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `academy_category`
---
-
+-- ----------------------------
+-- Table structure for academy_category
+-- ----------------------------
 DROP TABLE IF EXISTS `academy_category`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `academy_category` (
-  `acadeny_category_id` int NOT NULL AUTO_INCREMENT,
-  `acadeny_category_name` varchar(100) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` varchar(100) NOT NULL,
-  `updated_at` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `updated_by` varchar(100) NOT NULL,
-  PRIMARY KEY (`acadeny_category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `academy_category`  (
+  `acadeny_category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `acadeny_category_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp(0) NULL DEFAULT NULL,
+  `updated_at` timestamp(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0),
+  `created_by` int(11) NULL DEFAULT NULL,
+  `updated_by` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`acadeny_category_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `academy_category`
---
-
-LOCK TABLES `academy_category` WRITE;
-/*!40000 ALTER TABLE `academy_category` DISABLE KEYS */;
-/*!40000 ALTER TABLE `academy_category` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `academy_outline`
---
-
+-- ----------------------------
+-- Table structure for academy_outline
+-- ----------------------------
 DROP TABLE IF EXISTS `academy_outline`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `academy_outline` (
-  `academy_outline_id` int NOT NULL AUTO_INCREMENT,
-  `academy_id` int NOT NULL,
-  `content` text NOT NULL,
-  PRIMARY KEY (`academy_outline_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `academy_outline`  (
+  `academy_outline_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Mã đề cương khoá học',
+  `academy_id` int(11) NOT NULL COMMENT 'Mã khoá học',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Nội dung',
+  `created_at` timestamp(0) NULL DEFAULT current_timestamp(0),
+  `updated_at` timestamp(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`academy_outline_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `academy_outline`
---
-
-LOCK TABLES `academy_outline` WRITE;
-/*!40000 ALTER TABLE `academy_outline` DISABLE KEYS */;
-/*!40000 ALTER TABLE `academy_outline` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `academy_register_like`
---
-
+-- ----------------------------
+-- Table structure for academy_register_like
+-- ----------------------------
 DROP TABLE IF EXISTS `academy_register_like`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `academy_register_like` (
-  `academy_register_like_id` int NOT NULL AUTO_INCREMENT,
-  `student_id` int NOT NULL,
-  `academy_id` int NOT NULL,
-  `is_like` tinyint(1) NOT NULL DEFAULT '0',
-  `is_register` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`academy_register_like_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `academy_register_like`  (
+  `academy_register_like_id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL,
+  `academy_id` int(11) NOT NULL,
+  `is_like` tinyint(1) NOT NULL DEFAULT 0,
+  `is_register` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` timestamp(0) NULL DEFAULT current_timestamp(0),
+  `updated_at` timestamp(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`academy_register_like_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `academy_register_like`
---
-
-LOCK TABLES `academy_register_like` WRITE;
-/*!40000 ALTER TABLE `academy_register_like` DISABLE KEYS */;
-/*!40000 ALTER TABLE `academy_register_like` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `evaluate`
---
-
+-- ----------------------------
+-- Table structure for evaluate
+-- ----------------------------
 DROP TABLE IF EXISTS `evaluate`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `evaluate` (
-  `evaluate_id` int NOT NULL AUTO_INCREMENT,
-  `student_id` int NOT NULL,
-  `academy_id` int NOT NULL,
-  `point` int NOT NULL,
-  `comment` text,
-  PRIMARY KEY (`evaluate_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `evaluate`  (
+  `evaluate_id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) NOT NULL,
+  `academy_id` int(11) NOT NULL,
+  `point` int(11) NOT NULL,
+  `comment` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp(0) NULL DEFAULT current_timestamp(0),
+  `updated_at` timestamp(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0),
+  `updated_by` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`evaluate_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `evaluate`
---
-
-LOCK TABLES `evaluate` WRITE;
-/*!40000 ALTER TABLE `evaluate` DISABLE KEYS */;
-/*!40000 ALTER TABLE `evaluate` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `refresh_token` varchar(100) DEFAULT NULL,
-  `email` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `phone_number` varchar(15) DEFAULT NULL,
-  `birthday` date DEFAULT NULL,
-  `money` bigint NOT NULL DEFAULT '0',
-  `gender` varchar(5) NOT NULL,
-  `avatar` varchar(100) DEFAULT NULL,
-  `role` varchar(100) NOT NULL DEFAULT 'student',
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `user`  (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `refresh_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_number` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `birthday` date NULL DEFAULT NULL,
+  `money` bigint(20) NOT NULL DEFAULT 0,
+  `gender` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatar` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `role` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'student',
+  `created_at` timestamp(0) NULL DEFAULT current_timestamp(0),
+  `updated_at` timestamp(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping routines for database 'academy'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-06-04  0:41:23
+SET FOREIGN_KEY_CHECKS = 1;
